d69a7ed0278d4341ae2beff4cc33fad1aa8ebbfd
Antoine <dovodjaanto@gmail.com>
2025-10-24 13:47:29 +0200

    Backtrace of dtw matrix with terminal output

--- dynamic_time_warping.py
+++ dynamic_time_warping.py
@@ -1,15 +1,21 @@
 import random
 import matplotlib.pyplot as plt
 
-def create_timeseries(length = 7):
+random.seed(42)
+BOLD_RED = "\033[1;31m"
+RESET = "\033[0m"
+
+
+def create_timeseries(length=7):
     vals = []
     for i in range(length):
         vals.append(random.randint(3, 20))
-    print(vals)
     return vals
 
-def create_offset_timeseries(timeseries, offset = 3):
+
+def create_offset_timeseries(timeseries, offset=3):
     pass
+
 
 def compute_dtw_matrix(timeseries):
     if len(timeseries) != 2:
@@ -18,39 +24,73 @@ def compute_dtw_matrix(timeseries):
     n = len(timeseries[0])
     m = len(timeseries[1])
 
-    matrix = [[float('inf') for j in range(m+1)] for i in range(n+1)]
-    matrix[0][0] = 0 # dp base case
+    matrix = [[float("inf") for j in range(m + 1)] for i in range(n + 1)]
+    matrix[0][0] = 0  # dp base case
 
-    for i in range(1,n+1):
-        for j in range(1,m+1):
-           cost = abs(timeseries[0][i-1] - timeseries[1][j-1])
-           matrix[i][j] = cost + min(matrix[i-1][j],
-                                     matrix[i][j-1],
-                                     matrix[i-1][j-1])
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            cost = abs(timeseries[0][i - 1] - timeseries[1][j - 1])
+            matrix[i][j] = cost + min(
+                matrix[i - 1][j], matrix[i][j - 1], matrix[i - 1][j - 1]
+            )
 
     # adding ts-values into the base case cols/rows for nicer representation
     matrix[0][1:] = timeseries[0]
-    for i in range(1, n+1):
-        matrix[i][0] = timeseries[1][i-1]
+    for i in range(1, n + 1):
+        matrix[i][0] = timeseries[1][i - 1]
+
+    return matrix
+
+
+def backtrace_dtw(matrix):
+    path = []
+    i, j = (len(matrix) - 1, len(matrix[1]) - 1)
+    while i > 1 or j > 1:
+        path.append((i, j))
+
+        if i > 1 and j > 1:
+            min_val = min(matrix[i - 1][j], matrix[i][j - 1], matrix[i - 1][j - 1])
+            print(min_val)
+            if min_val == matrix[i - 1][j - 1]:
+                i, j = i - 1, j - 1
+            elif min_val == matrix[i - 1][j]:
+                i -= 1
+            else:
+                j -= 1
+
+        elif i > 1:
+            i -= 1
+        else:
+            j -= 1
+
+    path.append((1, 1))
+    path.reverse()
 
     print("Matrix:")
-    for row in matrix:
-        for col in row:
-            print(f"{col:>2}", end=" ")
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            print(
+                (
+                    f"{BOLD_RED}{matrix[i][j]:>2}{RESET}"
+                    if (i, j) in path
+                    else f"{matrix[i][j]:>2}"
+                ),
+                end=" ",
+            )
         print("")
 
+
 def plot_timeseries(timeseries: list[list[int]]):
-    plt.plot(timeseries[0], label='Timeseries 1')
-    plt.plot(timeseries[1], label='Timeseries 2')
-    plt.xlabel('Time Points')
-    plt.ylabel('Values')
+    plt.plot(timeseries[0], label="Timeseries 1")
+    plt.plot(timeseries[1], label="Timeseries 2")
+    plt.xlabel("Time Points")
+    plt.ylabel("Values")
     plt.legend()
     plt.show()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     timeseries = [create_timeseries() for _ in range(2)]
-    compute_dtw_matrix(timeseries)
+    dtw_mat = compute_dtw_matrix(timeseries)
+    backtrace_dtw(dtw_mat)
     plot_timeseries(timeseries)
-
-
-    
