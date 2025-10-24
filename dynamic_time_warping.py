b18b8dd97f3eaccd222b7217878a8fc40750ad2c
Antoine <dovodjaanto@gmail.com>
2025-10-24 12:26:40 +0200

    Starting with DTW matrix

--- dynamic_time_warping.py
+++ dynamic_time_warping.py
@@ -10,7 +10,14 @@ def create_timeseries(length = 7):
 
 def create_offset_timeseries(timeseries, offset = 3):
     pass
- 
+
+def compute_dtw_matrix(timeseries):
+    if len(timeseries) != 2:
+        print(f"Error: function was given {len(timeseries)} timeseries")
+
+    matrix = [[0 for n in range(len(timeseries[0]))] for m in range (len(timeseries[1]))]
+    for row in matrix:
+        print(row)
 
 def plot_timeseries(timeseries: list[list[int]]):
     plt.plot(timeseries[0], label='Timeseries 1')
@@ -22,6 +29,8 @@ def plot_timeseries(timeseries: list[list[int]]):
 
 if __name__ == '__main__':
     timeseries = [create_timeseries() for _ in range(2)]
+    compute_dtw_matrix(timeseries)
     plot_timeseries(timeseries)
+
 
     
